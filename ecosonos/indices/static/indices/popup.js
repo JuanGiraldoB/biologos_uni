a4f774a5ccee21d30ad5ce01aeb673b1414c03e1
JuanGiraldoB <jugiraldob@unal.edu.co>
2024-02-02 16:33:14 -0500

    added popup logic to avoid page refresh

---
+++ ecosonos/indices/static/indices/popup.js
@@ -0,0 +1,9 @@
+document.getElementById("test2").addEventListener("click", () => {
+    document.getElementById("popup10").style.display = "block";
+    console.log("gakdmgklasdmfgklasmfklasdmlkas")
+})
+
+document.getElementById("test").addEventListener("click", (event) => {
+    document.getElementById("popup10").style.display = "none";
+    event.preventDefault();
+})
