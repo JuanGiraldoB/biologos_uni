f48b931957698ce0a4f48d2d8ecec3d9d3b99da7
JuanGiraldoB <jugiraldob@unal.edu.co>
2024-02-15 10:36:05 -0500

    truncated db

--- ecosonos/etiquetado_auto/admin.py
+++ ecosonos/etiquetado_auto/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import GuardadoClusterResult, MetodologiaResult
 # Register your models here.
+
+admin.site.register(GuardadoClusterResult)
+admin.site.register(MetodologiaResult)
